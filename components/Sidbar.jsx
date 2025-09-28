9034ce445d4d9d91232dfd1fd913ebaca01f0db6
3oheil Rostami <soheilrostami449@gmail.com>
2025-09-28 21:11:05 +0330

    add context to use them in another components

--- components/Sidbar.jsx
+++ components/Sidbar.jsx
@@ -1,16 +1,19 @@
+import { ThemContext } from '@/app/layout'
 import Link from 'next/link'
+import { useContext } from 'react'
 
 export default function Sidbar() {
+  const {theme} = useContext(ThemContext)
   return (
     <div>
       <aside style={{margin:'10px', padding:'10px', border:'1px solid', height:'85%' , borderRadius:'5px' , display:'flex', justifyContent:'center', alignContent : 'center'}}>
         <menu>
             <ul style={{display:'flex', flexDirection:'column', margin:'10px', gap:'10px'}}>
-                <Link href='/'>Home</Link>
-                <Link href={'/contact'}>Contact</Link>
-                <Link href={'/about'}>About</Link>
-                <Link href={'/dashboard'}>Dashboard</Link>
-                <Link href={'/dashboard/setting'}> Setting</Link>
+                <Link style={theme ? {color:'black' , transition:'all 0.5s'} : {color:'white' , transition:'all 0.5s'}}  href='/'>Home</Link>
+                <Link style={theme ? {color:'black' , transition:'all 0.5s'} : {color:'white' , transition:'all 0.5s'}}  href={'/contact'}>Contact</Link>
+                <Link style={theme ? {color:'black' , transition:'all 0.5s'} : {color:'white' , transition:'all 0.5s'}}  href={'/about'}>About</Link>
+                <Link style={theme ? {color:'black' , transition:'all 0.5s'} : {color:'white' , transition:'all 0.5s'}}  href={'/dashboard'}>Dashboard</Link>
+                <Link style={theme ? {color:'black' , transition:'all 0.5s'} : {color:'white' , transition:'all 0.5s'}}  href={'/dashboard/setting'}> Setting</Link>
                 
             </ul>
         </menu>
@@ -18,3 +21,4 @@ export default function Sidbar() {
     </div>
   )
 }
+ 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
 'use client';
 
-import { useEffect, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Sidbar from "../../components/Sidbar";
 import { usePathname } from "next/navigation";
 import '../../styles/global.css'
+export const ThemContext = createContext()
+
 export default function RootLayout({ children }) {
   const [theme, setTheme] = useState(false); // ✅ داخل تابع کامپوننت
   const inputRef = useRef()
   const pathname = usePathname()
 
+
+  const [name , setName] = useState('Soheil')
   const hideFooter = ['/dashboard']
 
   const shouldHideFooter = hideFooter.includes(pathname)
@@ -22,26 +26,30 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body style={theme ? { backgroundColor: 'white', color: 'black', transition: 'all 1s' } : { backgroundColor: 'black', color: 'white', transition: 'all 1s' }}>
-        <div style={{ textAlign: 'center' }}>
-        </div>
-        {/* <Header onClickk={() => alert('cll')}>Are you sure ? </Header> */}
-        <Header > </Header>
+      <ThemContext.Provider value={{theme , name}}>
+        <body style={theme ? { backgroundColor: 'white', color: 'black', transition: 'all 1s' } : { backgroundColor: 'black', color: 'white', transition: 'all 1s' }}>
+          <div style={{ textAlign: 'center' }}>
+          </div>
+          {/* <Header onClickk={() => alert('cll')}>Are you sure ? </Header> */}
+          <Header > </Header>
 
-        <div style={{ textAlign: 'center' }}>
-          <button onClick={themClick}>{theme ? 'Light' : 'Dark'}</button>
+          <div style={{ textAlign: 'center' }}>
+            <button onClick={themClick}>{theme ? 'Light' : 'Dark'}</button>
 
-        </div>
-        <section style={{ display: 'flex' }}>
-          <Sidbar />
-          <article>
-            {children}
+          </div>
+          <section style={{ display: 'flex' }}>
+            <Sidbar />
+            <article>
+              {children}
 
-          </article>
-        </section>
+            </article>
+          </section>
 
-        {!shouldHideFooter && <Footer />}
-      </body>
+
+          {!shouldHideFooter && <Footer />}
+
+        </body>
+      </ThemContext.Provider>
     </html>
   );
 }
