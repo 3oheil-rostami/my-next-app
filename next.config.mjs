53a156ea2de7df6701dc120c08eff9eeaf79ba31
3oheil Rostami <soheilrostami449@gmail.com>
2025-09-29 18:20:05 +0330

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // مهم: خروجی استاتیک می‌سازه (برای GitHub Pages لازمه)
+  basePath: "/my-next-app", // اسم ریپو
+  assetPrefix: "/my-next-app/", // آدرس فایل‌های استاتیک
+};
+
+module.exports = nextConfig;
 export default nextConfig;
+// next.config.js
